3c690caba590da19a54dbea1dcaab78e28aa6a5f
oyerohabib <oyerohabib@yahoo.com>
2024-02-16 04:19:20 +0100

    feat: exported all lib from index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,33 @@
+import {
+    timerSettingsAtom,
+    timerModeAtom,
+    longBreakIntervalAtom,
+    todosAtom,
+    unfininishedTodoSessionsAmountAtom,
+    finishedSessionsAtom,
+    getTotalSessionsAtom,
+    getUnfinishedSessionsForEachTimerModeAtom,
+    getTotalTimeInMinutesAtom,
+    getUpdatedTimeAtom,
+    selectedTodoIdAtom,
+    themeSettingsAtom,
+} from './atom';
+
+export {
+    timerSettingsAtom,
+    timerModeAtom,
+    longBreakIntervalAtom,
+    todosAtom,
+    unfininishedTodoSessionsAmountAtom,
+    finishedSessionsAtom,
+    getTotalSessionsAtom,
+    getUnfinishedSessionsForEachTimerModeAtom,
+    getTotalTimeInMinutesAtom,
+    getUpdatedTimeAtom,
+    selectedTodoIdAtom,
+    themeSettingsAtom,
+};
+
+import { runTour } from './driver';
+
+export { runTour };
